f4a191c907a6afd261666e6ffe0d8719f6e0e64b
mitom18 <michal.toman99@email.cz>
2021-04-01 16:17:37 +0200

    Using express and MongoDB

--- server.js
+++ server.js
@@ -1,36 +1,51 @@
-const fs = require("fs");
 const path = require("path");
-const jsonServer = require("json-server");
-const dbFolderPath = path.join(__dirname, "data");
-const dbFilePath = path.join(dbFolderPath, "db.json");
-if (!fs.existsSync(dbFolderPath)) {
-    fs.mkdirSync(dbFolderPath);
-}
-if (!fs.existsSync(dbFilePath)) {
-    fs.writeFileSync(dbFilePath, JSON.stringify({ answers: [] }));
-}
-const server = jsonServer.create();
-const router = jsonServer.router(dbFilePath);
-const middleware = jsonServer.defaults({
-    static: path.join(__dirname, "build"),
-});
-const port = Number(process.env.PORT || 3000);
+const express = require("express");
+const { MongoClient } = require("mongodb");
 
-server.use(middleware);
-server.use((req, res, next) => {
-    if (req.method !== "GET" && req.method !== "POST") {
-        res.sendStatus(405);
-    } else {
-        next();
+const server = express();
+server.use(express.static(path.join(__dirname, "build")));
+server.use(express.json());
+const port = Number(process.env.PORT || 3000);
+const mongoDbUri = process.env.MONGODB_URI;
+
+server.get("/api/answers", async function (req, res) {
+    const client = new MongoClient(mongoDbUri, { useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const database = client.db("supi_orientak");
+        const collection = database.collection("answers");
+
+        const answers = await collection.find().toArray();
+
+        return res.json(answers);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await client.close();
     }
 });
-server.use("/api", router);
-server.get("/service-worker.js", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "service-worker.js"));
+
+server.post("/api/answers", async function (req, res) {
+    const client = new MongoClient(mongoDbUri, { useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const database = client.db("supi_orientak");
+        const collection = database.collection("answers");
+
+        const dbResponse = await collection.insertOne(req.body);
+
+        return res.status(201).json(dbResponse.ops[0]);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await client.close();
+    }
 });
-server.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+
 server.listen(port, () => {
-    console.log("JSON Server is running");
+    console.log("Server is running");
 });
